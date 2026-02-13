e5a5acf11198c96b395f9ae29736022bf31a6bad
seleneamengual-arch <seleneamengual@paucasesnovescifp.cat>
2026-02-13 22:55:52 +0100

    Refactorización del paquete cuentas, encapsulación y método operativa_cuenta

---
+++ cuentas/Main.java
@@ -0,0 +1,29 @@
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float cantidad = 695;
+        CCuenta cuenta1;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta(cuenta1, cantidad);
+    }
+
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        double saldoActual;
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
